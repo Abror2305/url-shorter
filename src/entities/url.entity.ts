f39a677be515fe0a442842818a64d681de0c0acf
Abror2305 <abroralisherov90@gmail.com>
2022-06-28 20:30:38 +0500

    User entity completed

--- src/entities/url.entity.ts
+++ src/entities/url.entity.ts
@@ -1,7 +1,23 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
+import * as shortid from 'shortid';
 
 @Schema({ timestamps: { createdAt: true, updatedAt: true } })
-export class Urls extends Document {}
+export class Urls extends Document {
+  @Prop({ type: Types.ObjectId, default: shortid.generate })
+  _id: Types.ObjectId;
+  @Prop({ default: 0 })
+  clicks: number;
+  @Prop()
+  maxClicks: number;
+  @Prop({ required: true })
+  redirectUrl: string;
+  @Prop()
+  qrCodePath: string;
+  @Prop({ type: Date })
+  expiresIn: Date;
+  @Prop({ type: Types.ObjectId, ref: 'Users' })
+  userId: Types.ObjectId;
+}
 
 export const UrlSchema = SchemaFactory.createForClass(Urls);
